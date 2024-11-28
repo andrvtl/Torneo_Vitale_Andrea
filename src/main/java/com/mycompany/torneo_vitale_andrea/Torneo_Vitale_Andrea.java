9b9a016a731109cc85327d6b04117546a90b9e18
thomas <r.monaccuia67@gmail.com>
2024-11-28 09:04:46 +0100

    Commit di configurazione

---
+++ src/main/java/com/mycompany/torneo_vitale_andrea/Torneo_Vitale_Andrea.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.torneo_vitale_andrea;
+
+/**
+ *
+ * @author B045_03
+ */
+public class Torneo_Vitale_Andrea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
